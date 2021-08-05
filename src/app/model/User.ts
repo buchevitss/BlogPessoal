345c43907b035f9f4b01863be2cb61c7a13ee04f
buchevitss <thais.buchweitz@outlook.com>
2021-08-05 01:17:43 -0300

    rotas

---
+++ src/app/model/User.ts
@@ -0,0 +1,9 @@
+import { Postagem } from "./Postagem"
+
+export class User{
+    public id: number
+    public nome: string
+    public usuario: string
+    public senha: string
+    public postagem: Postagem[]
+}
